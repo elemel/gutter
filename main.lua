40f7f7067edff2349fdf3da27f5c30934ec48844
Mikael Lind <elemel@elemel.se>
2020-07-02 11:05:17 +0200

    Add spinning triangle splat of death

---
+++ main.lua
@@ -0,0 +1,42 @@
+function love.load(arg)
+  love.window.setTitle("Splatter")
+
+  love.window.setMode(800, 600, {
+    highdpi = true,
+    resizable = true,
+  })
+
+  local vertices = {
+    {2, 0, 2, 0, 1, 0, 0, 1},
+    {-1, 1.7320508075688774, -1, 1.7320508075688774, 0, 1, 0, 1},
+    {-1, -1.7320508075688774, -1, -1.7320508075688774, 0, 0, 1, 1},
+  }
+
+  mesh = love.graphics.newMesh(vertices, triangles)
+
+  shader = love.graphics.newShader([[
+    vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
+    {
+      if (texture_coords.x * texture_coords.x + texture_coords.y * texture_coords.y > 1 * 1) {
+        discard;
+      }
+
+      return color;
+    }
+  ]])
+end
+
+function love.draw()
+  local width, height = love.graphics.getDimensions()
+  love.graphics.translate(0.5 * width, 0.5 * height)
+  love.graphics.scale(0.25 * height)
+  love.graphics.rotate(love.timer.getTime())
+
+  love.graphics.setColor(1, 1, 1, 0.5)
+  love.graphics.draw(mesh)
+
+  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.setShader(shader)
+  love.graphics.draw(mesh)
+  love.graphics.setShader(nil)
+end
