8f6a15f539f32e30e860bb04675a7e040a854c69
Mikael Lind <elemel@elemel.se>
2020-07-03 11:29:02 +0200

    Draw tangent space for random points on sphere

--- main.lua
+++ main.lua
@@ -1,3 +1,47 @@
+local function normalize3(x, y, z)
+  local length = math.sqrt(x * x + y * y + z * z)
+  return x / length, y / length, z / length
+end
+
+-- https://math.stackexchange.com/a/1586011
+local function randomPointOnUnitSphere(random)
+  random = random or love.math.random
+
+  while true do
+    local x = 2 * random() - 1
+    local y = 2 * random() - 1
+    local z = 2 * random() - 1
+
+    if x * x + y * y + z * z < 1 then
+      return normalize3(x, y, z)
+    end
+  end
+end
+
+local function perp3(x, y, z)
+  if math.abs(x) < math.abs(y) then
+    if math.abs(y) < math.abs(z) then
+      return 0, -z, y
+    elseif math.abs(x) < math.abs(z) then
+      return 0, z, -y
+    else
+      return -y, x, 0
+    end
+  else
+    if math.abs(z) < math.abs(y) then
+      return y, -x, 0
+    elseif math.abs(z) < math.abs(x) then
+      return z, 0, -x
+    else
+      return -z, 0, x
+    end
+  end
+end
+
+local function cross3(ax, ay, az, bx, by, bz)
+  return ay * bz - az * by, az * bx - ax * bz, ax * by - ay * bx
+end
+
 function love.load(arg)
   love.window.setTitle("Gutter")
 
@@ -24,13 +68,26 @@ function love.load(arg)
       return color;
     }
   ]])
+
+  points = {}
+
+  for _ = 1, 256 do
+    local x, y, z = randomPointOnUnitSphere()
+    table.insert(points, 2 * x)
+    table.insert(points, 2 * y)
+    table.insert(points, 2 * z)
+  end
 end
 
 function love.draw()
   local width, height = love.graphics.getDimensions()
   love.graphics.translate(0.5 * width, 0.5 * height)
-  love.graphics.scale(0.25 * height)
-  love.graphics.rotate(love.timer.getTime())
+
+  local scale = 0.25 * height
+  love.graphics.scale(scale)
+  love.graphics.setLineWidth(1 / scale)
+
+  -- love.graphics.rotate(love.timer.getTime())
 
   love.graphics.setColor(1, 1, 1, 0.5)
   love.graphics.draw(mesh)
@@ -39,6 +96,28 @@ function love.draw()
   love.graphics.setShader(shader)
   love.graphics.draw(mesh)
   love.graphics.setShader(nil)
+
+  local vectorScale = 0.25
+
+  for i = 1, #points, 3 do
+    local x = points[i]
+    local y = points[i + 1]
+    local z = points[i + 2]
+
+    if z < 0 then
+      local nx, ny, nz = normalize3(x, y, z)
+      love.graphics.setColor(1, 0, 0, 1)
+      love.graphics.line(x, y, x + vectorScale * nx, y + vectorScale * ny)
+
+      local tx, ty, tz = perp3(nx, ny, nz)
+      love.graphics.setColor(0, 1, 0, 1)
+      love.graphics.line(x, y, x + vectorScale * tx, y + vectorScale * ty)
+
+      local bx, by, bz = cross3(nx, ny, nz, tx, ty, tz)
+      love.graphics.setColor(0, 0, 1, 1)
+      love.graphics.line(x, y, x + vectorScale * bx, y + vectorScale * by)
+    end
+  end
 end
 
 function love.keypressed(key, scancode, isrepeat)
