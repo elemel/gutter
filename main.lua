8887b6be37a546d667bc73c798cac9bb64a20246
Mikael Lind <elemel@elemel.se>
2020-07-07 00:54:37 +0200

    Tweak colors

--- main.lua
+++ main.lua
@@ -386,7 +386,7 @@ function love.load(arg)
         inverseTransform = love.math.newTransform(-0.5, -0.25):inverse(),
         scale = 0.5,
         smoothRadius = 0,
-        color = {0.25, 1, 0.125, 1},
+        color = {0.5, 1, 0.25, 1},
       },
 
       {
@@ -395,7 +395,7 @@ function love.load(arg)
         inverseTransform = love.math.newTransform(0.5, 0.25):inverse(),
         scale = 0.75,
         smoothRadius = 0.5,
-        color = {0.125, 0.5, 1, 1},
+        color = {0.25, 0.75, 1, 1},
       },
 
       {
@@ -404,7 +404,7 @@ function love.load(arg)
         inverseTransform = translate3(love.math.newTransform(), 0, -0.25, 0.5):inverse(),
         scale = 0.5,
         smoothRadius = 0.25,
-        color = {1, 0.25, 0.125, 1},
+        color = {1, 0.5, 0.25, 1},
       },
     }
   }
