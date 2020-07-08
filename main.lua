91027aff5525f84e0ffb5380c47214449356329b
Mikael Lind <elemel@elemel.se>
2020-07-08 12:24:58 +0200

    Tweak timer formatting

--- main.lua
+++ main.lua
@@ -423,7 +423,7 @@ function love.load(arg)
   local time = love.timer.getTime()
   mesh = newMeshFromEdits(sculpture.edits, minX, minY, minZ, maxX, maxY, maxZ, sizeX, sizeY, sizeZ)
   time = love.timer.getTime() - time
-  print(string.format("Total: Converted model to mesh in %.3f seconds.", time))
+  print(string.format("Total: Converted model to mesh in %.3f seconds", time))
 end
 
 local function debugDrawPointBases(points)
