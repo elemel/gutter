5df1ac783ef76b45944b2113c999e6ac27f1be4a
Mikael Lind <elemel@elemel.se>
2020-07-28 23:38:16 +0200

    Merge dump functions

--- gutter/dump.lua
+++ gutter/dump.lua
@@ -24,63 +24,34 @@ local keywords = {
   ["while"] = true,
 }
 
-function M.dumpTable(t, format, buffer, depth, stack)
-  local pretty = format == "pretty"
+function M.dump(value, format, buffer, depth, stack)
+  format = format or "compact"
+  assert(format == "compact" or format == "pretty", "Invalid format")
 
-  if stack[t] then
-    table.insert(buffer, "(")
-    table.insert(buffer, tostring(stack[t]))
-    table.insert(buffer, ")")
-    return
-  end
+  buffer = buffer or {}
+  depth = depth or 0
 
-  table.insert(stack, t)
-  stack[t] = #stack
-  local first = true
-  local blank1 = false
-  local blank2 = false
-  table.insert(buffer, "{")
+  if type(value) == "string" then
+    table.insert(buffer, string.format("%q", value))
+  elseif type(value) == "table" then
+    stack = stack or {}
+    local pretty = format == "pretty"
 
-  for i, element in ipairs(t) do
-    blank2 = type(element) == "table" and not stack[element]
-
-    if first then
-      if pretty then
-        table.insert(buffer, "\n")
-
-        for j = 1, depth + 1 do
-          table.insert(buffer, "  ")
-        end
-      end
-    else
-      if pretty then
-        if blank1 or blank2 then
-          table.insert(buffer, "\n")
-        end
-
-        for j = 1, depth + 1 do
-          table.insert(buffer, "  ")
-        end
-      else
-        table.insert(buffer, ",")
-      end
+    if stack[value] then
+      table.insert(buffer, "(")
+      table.insert(buffer, tostring(stack[value]))
+      table.insert(buffer, ")")
+      return
     end
 
-    M.dumpValue(element, format, buffer, depth + 1, stack)
+    table.insert(stack, value)
+    stack[value] = #stack
+    local first = true
+    local blank1 = false
+    local blank2 = false
+    table.insert(buffer, "{")
 
-    if pretty then
-      table.insert(buffer, ",\n")
-    end
-
-    first = false
-    blank1 = blank2
-  end
-
-  for name, element in pairs(t) do
-    local isArrayIndex = type(name) == "number" and name <= #t
-    local isMetamethodName = type(name) == "string" and string.sub(name, 1, 2) == "__"
-
-    if not isArrayIndex and not isMetamethodName then
+    for i, element in ipairs(value) do
       blank2 = type(element) == "table" and not stack[element]
 
       if first then
@@ -105,64 +76,86 @@ function M.dumpTable(t, format, buffer, depth, stack)
         end
       end
 
-      if type(name) == "string" and
-          string.find(name, "^[%a_][%w_]*$") and
-          not keywords[name] then
-
-        table.insert(buffer, name)
-      else
-        table.insert(buffer, "[")
-        M.dumpValue(name, format, buffer, depth + 1, stack)
-        table.insert(buffer, "]")
-      end
+      M.dump(element, format, buffer, depth + 1, stack)
 
       if pretty then
-        table.insert(buffer, " = ")
-      else
-        table.insert(buffer, "=")
-      end
-
-      M.dumpValue(element, format, buffer, depth + 1, stack)
-
-      if pretty then
-          table.insert(buffer, ",\n")
+        table.insert(buffer, ",\n")
       end
 
       first = false
       blank1 = blank2
     end
-  end
 
-  if not first and pretty then
-    for j = 1, depth do
-      table.insert(buffer, "  ")
+    for name, element in pairs(value) do
+      local isArrayIndex = type(name) == "number" and name <= #value
+      local isMetamethodName = type(name) == "string" and string.sub(name, 1, 2) == "__"
+
+      if not isArrayIndex and not isMetamethodName then
+        blank2 = type(element) == "table" and not stack[element]
+
+        if first then
+          if pretty then
+            table.insert(buffer, "\n")
+
+            for j = 1, depth + 1 do
+              table.insert(buffer, "  ")
+            end
+          end
+        else
+          if pretty then
+            if blank1 or blank2 then
+              table.insert(buffer, "\n")
+            end
+
+            for j = 1, depth + 1 do
+              table.insert(buffer, "  ")
+            end
+          else
+            table.insert(buffer, ",")
+          end
+        end
+
+        if type(name) == "string" and
+            string.find(name, "^[%a_][%w_]*$") and
+            not keywords[name] then
+
+          table.insert(buffer, name)
+        else
+          table.insert(buffer, "[")
+          M.dump(name, format, buffer, depth + 1, stack)
+          table.insert(buffer, "]")
+        end
+
+        if pretty then
+          table.insert(buffer, " = ")
+        else
+          table.insert(buffer, "=")
+        end
+
+        M.dump(element, format, buffer, depth + 1, stack)
+
+        if pretty then
+            table.insert(buffer, ",\n")
+        end
+
+        first = false
+        blank1 = blank2
+      end
     end
-  end
 
-  table.insert(buffer, "}")
-  stack[t] = nil
-  assert(table.remove(stack) == t)
-end
+    if not first and pretty then
+      for j = 1, depth do
+        table.insert(buffer, "  ")
+      end
+    end
 
-function M.dumpValue(value, format, buffer, depth, stack)
-  if type(value) == "string" then
-    table.insert(buffer, string.format("%q", value))
-  elseif type(value) == "table" then
-    M.dumpTable(value, format, buffer, depth, stack)
+    table.insert(buffer, "}")
+    stack[value] = nil
+    assert(table.remove(stack) == value)
   else
     table.insert(buffer, tostring(value))
   end
-end
 
-function M.dump(value, format, buffer, depth, stack)
-  format = format or "compact"
-  assert(format == "compact" or format == "pretty", "Invalid format")
-
-  buffer = buffer or {}
-  depth = depth or 0
-  stack = stack or {}
-
-  M.dumpValue(value, format, buffer, depth, stack)
   return buffer
 end
 
